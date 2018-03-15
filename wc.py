cda03fa6b64dab3537fd247f1a6cf0070b9fc389
JustynaGacek <33673000+JustynaGacek@users.noreply.github.com>
2018-03-15 20:28:05 +0100

    Add files via upload

---
+++ wc.py
@@ -0,0 +1,40 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Count letters, words and lines in text file.')
+parser.add_argument('filename', help="name of input file")
+parser.add_argument('-m', '--chars', help="count letters", action='store_true')
+parser.add_argument('-w', '--words', help="count words", action='store_true')
+parser.add_argument('-l', '--lines', help="count lines", action='store_true')
+args = parser.parse_args()
+
+try:
+    file = open(args.filename)
+
+    chars_counter = 0
+    words_counter = 0
+    lines_counter = 0
+
+    for line in file:
+        for letter in line:
+            chars_counter += 1
+
+        list_of_words = line.split()
+        words_counter += len(list_of_words)
+
+        lines_counter += 1
+
+    file.close()
+
+
+    if args.chars:
+        print("Number of chars: %d " % chars_counter)
+
+    if args.words:
+        print("Number of words: %d " % words_counter)
+
+    if args.lines:
+        print("Number of lines: %d" % lines_counter)
+
+except IOError:
+    print("The name of file is incorrect or this file does not exist. Try to use other name of file.")
